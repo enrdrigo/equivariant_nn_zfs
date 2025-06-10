75886cda85f76f8ec91379545617598f5d186529
endrigo <enr.drigo@gmail.com>
2025-06-10 11:08:52 -0500

    fixed device allocation in get_graph_edge_attributes

--- equivariant_nn_zfs/model/model.py
+++ equivariant_nn_zfs/model/model.py
@@ -39,10 +39,6 @@ class TensorRegressor(nn.Module):
         super().__init__()
         self.device = device if device is not None else torch.device('cpu')
 
-        print(self.device, 'deviceeeeeeee')
-
-        radial_cutoff = torch.tensor(radial_cutoff, device=self.device)
-
         self.cutoff = PolynomialCutoff(r_max=radial_cutoff, p=pol_cut_num)
 
         self.cutoff = self.cutoff.to(self.device)
