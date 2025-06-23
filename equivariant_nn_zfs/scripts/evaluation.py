17ac25a3d5e62277114dca78da0b16fcb807e519
endrigo <enr.drigo@gmail.com>
2025-06-23 17:06:16 -0500

    create evaluation module

--- equivariant_nn_zfs/scripts/evaluation.py
+++ equivariant_nn_zfs/scripts/evaluation.py
@@ -12,6 +12,8 @@ from equivariant_nn_zfs.tools.convert_matrix import cartesian_to_spherical_irrep
 def evaluating(data: list,
                model: TensorRegressor,
                radial_cutoff=None,
+               irreps_out=None,
+               do_irreps = False,
                batch_size=10,
                num_workers=4,
                path_to_evaluate=None,
@@ -20,68 +22,13 @@ def evaluating(data: list,
     if path_to_evaluate is None:
         path_to_evaluate = 'evaluate_dataset.extxyz'
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    model = model.to(device)
-
-    model.device = device
-
-    dataset = EvaluationDataset(data,
-                                model=model,
-                                radial_cutoff=radial_cutoff
-                                )
-    loader = DataLoader(dataset,
-                        batch_size=batch_size,
-                        shuffle=False,
-                        num_workers=num_workers,
-                        collate_fn=collate_fn_eval
-                        )
-
-    y_pred_tot=[]
-    y_pred_local=[]
-
-    with torch.no_grad():
-        for batches in tqdm(loader, desc="Evaluating"):
-            batches_data = batches['batches'].to(model.device)
-
-            y_pred= model(batches_data)  # Forward pass
-
-            y_pred_tot.append(y_pred['target'][:, :].cpu())
-            y_pred_local.append(y_pred['local target'][:, :].cpu())
-
-    y_pred_tot = torch.cat(y_pred_tot, dim=0)
-    y_pred_local = torch.cat(y_pred_local, dim=0)
-
-    start_idx=0
-    for idx, data_ in enumerate(data):
-        end_idx=start_idx+len(data_)
-        data[idx].info['eval_target']=y_pred_tot[idx].numpy()
-        data[idx].arrays['eval_local_target'] = y_pred_local[start_idx:end_idx].numpy()
-        data[idx].arrays['eval_local_norm_target']=y_pred_local[start_idx:end_idx].norm(dim=1).numpy()
-        start_idx=end_idx
-
-    write(path_to_evaluate,data)
-
-    return
-
-def evaluating_for_comparison(data: list,
-                              model: TensorRegressor,
-                              irreps_out=None,
-                              radial_cutoff=None,
-                              batch_size=10,
-                              num_workers=4,
-                              path_to_evaluate=None,
-                              ):
-
-    if path_to_evaluate is None:
-        path_to_evaluate = 'evaluate_dataset.extxyz'
-
     if irreps_out is None:
-        irreps_out = model.irreps_out
+        try:
+            irreps_out = model.irreps_out
+        except: do_irreps = False
 
     assert isinstance(irreps_out, Irreps), "irreps_out must be an instance of Irreps"
 
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = model.to(device)
@@ -117,8 +64,10 @@ def evaluating_for_comparison(data: list,
     start_idx=0
     for idx, data_ in enumerate(data):
         end_idx=start_idx+len(data_)
-        data[idx].info['true_target']=cartesian_to_spherical_irreps(data[idx].info['target_L2'].reshape(3, 3), irreps=irreps_out).numpy()
         data[idx].info['eval_target']=y_pred_tot[idx].numpy()
+        if do_irreps:
+            data[idx].info['true_target'] = cartesian_to_spherical_irreps(data[idx].info['target_L2'].reshape(3, 3),
+                                                                          irreps=irreps).numpy()
         data[idx].arrays['eval_local_target'] = y_pred_local[start_idx:end_idx].numpy()
         data[idx].arrays['eval_local_norm_target']=y_pred_local[start_idx:end_idx].norm(dim=1).numpy()
         start_idx=end_idx
@@ -126,4 +75,3 @@ def evaluating_for_comparison(data: list,
     write(path_to_evaluate,data)
 
     return
-
